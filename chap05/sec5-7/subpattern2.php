e991bc5cd2bfb7b034f6186a6b3f8ef9e11ed924
SeiichiN <billie175@gmail.com>
2024-10-10 21:41:30 +0900

    subpattern2.php u修飾子について

--- chap05/sec5-7/subpattern2.php
+++ chap05/sec5-7/subpattern2.php
@@ -1,12 +1,12 @@
 <?php
-$pattern = "/\A(090|080|070)(-?\d{4}){2}\z/";  // uはダメ
+$pattern = "/(090|080|070)(-?\d{4}){2}/u";  // uはダメ
 
 var_dump(preg_match($pattern, "090-1234-5678")); # 1
-var_dump(preg_match($pattern, "090-１２３４-５６７８")); # 0
 var_dump(preg_match($pattern, "080-1234-5678")); # 1
-var_dump(preg_match($pattern, "080-１２３４-５６７８")); # 0
 var_dump(preg_match($pattern, "07012345678")); # 1
 var_dump(preg_match($pattern, "12345678")); # 0
-var_dump(preg_match($pattern, "電話090-1234-5678（携帯）")); # 0
+// var_dump(preg_match($pattern, "電話090-1234-5678（携帯）")); # 0
 
+var_dump(preg_match($pattern, "090-１２３４-５６７８")); # 0
+var_dump(preg_match($pattern, "080-１２３４-５６７８")); # 0
 
