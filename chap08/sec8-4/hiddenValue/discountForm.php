6dc52cd42120c572174d775cfaec4b8b9b001997
SeiichiN <billie175@gmail.com>
2025-10-21 09:13:51 +0900

    discountForm.php modified.

--- chap08/sec8-4/hiddenValue/discountForm.php
+++ chap08/sec8-4/hiddenValue/discountForm.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <title>Document</title>
   <link rel="stylesheet" href="../css/style.css">
 </head>
+
 <body>
   <div>
-  <?php
-  $discount = 0.8;
-  $off = (1 - $discount) * 100;
-  if ($discount > 0) {
-    echo "<h2>このページでのご購入は{$off}％ OFFになります</h2>";
-  }
-  $tanka = 2900;
-  $tanka_fmt = number_format($tanka);
-  ?>
-  <form action="discount.php" method="post">
-    <input type="hidden" name="discount" value="<?php echo $discount; ?>">
-    <input type="hidden" name="tanka" value="<?php echo $tanka; ?>">
-    <ul>
-      <li>単価：<?php echo $tanka_fmt; ?>円</li>
-      <li><label>個数：<input type="number" name="kosu"></label></li>
-      <li><input type="submit" value="計算する"></li>
-    </ul>
-  </form>
+    <?php
+    $discount = 0.8;
+    $off = (1 - $discount) * 100;
+    if ($discount > 0) {
+      echo "<h2>このページでのご購入は{$off}％ OFFになります</h2>";
+    }
+    $tanka = 2900;
+    $tanka_fmt = number_format($tanka);
+    ?>
+    <form action="discount.php" method="post">
+      <input type="hidden" name="discount" value="<?php echo $discount; ?>">
+      <input type="hidden" name="tanka" value="<?php echo $tanka; ?>">
+      <ul>
+        <li>単価：<?php echo $tanka_fmt; ?>円</li>
+        <li><label>個数：<input type="number" name="kosu"></label></li>
+        <li><input type="submit" value="計算する"></li>
+      </ul>
+    </form>
   </div>
 </body>
+
 </html>
