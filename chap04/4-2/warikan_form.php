776a208282bcfeb2657f1e0bff3ac7f39ed044f0
SeiichiN <billie175@gmail.com>
2026-04-16 19:45:18 +0900

    warikan_form.php modified.

--- chap04/4-2/warikan_form.php
+++ chap04/4-2/warikan_form.php
@@ -5,7 +5,7 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="4-2/warikan_return.php" method="post">
+  <form action="warikan_return.php" method="post">
     合計金額：<input type="text" name="total"><br>
     人数：<input type="text" name="ninzu"><br>
     <input type="submit" value="計算する">
