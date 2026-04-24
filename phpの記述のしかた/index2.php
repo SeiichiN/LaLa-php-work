f8f5483763d4e27935737dadacc1c9c5bdc19856
SeiichiN <billie175@gmail.com>
2026-04-24 16:27:06 +0900

    4/24 p552 次は更新

---
+++ phpの記述のしかた/index2.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+<?php $a = 10; ?>
+<?php if ($a > 0) { ?>
+  <?php echo '正の数です', '<br>'; ?>
+<?php } else { ?>
+  <?php echo '負の数です', '<br>'; ?>
+<?php } ?>
+  </body>
+</html>
+
+<!-- 修正時刻: Fri 2026/04/24 07:24:58 -->
