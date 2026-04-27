c0e400e72b97fecbadfed3eb9b73191f438052e7
SeiichiN <billie175@gmail.com>
2026-04-27 09:28:29 +0900

    chap13がなぜか抜けてたので、追加

---
+++ chap13/13-2/connect/PDO_testdb.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>データベースに接続する</title>
+  <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+  <div>
+    <?php
+    $user = "testuser";
+    $password = "testuser";
+    $dbName = "testdb";
+    $host = "localhost:3306";
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+    try {
+      $pdo = new PDO($dsn, $user, $password);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "データベース{$dbName}に接続しました。", PHP_EOL;
+      $pdo = NULL;
+    } catch (Exception $e) {
+      echo '<span class="error">エラーがありました</span><br>';
+      echo $e->getMessage();
+      exit();
+    }
+    ?>
+  </div>
+</body>
+</html>
