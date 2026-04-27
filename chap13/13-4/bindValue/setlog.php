7969ca152b2b3513e13539c6b97daa84889044b5
SeiichiN <billie175@gmail.com>
2026-04-28 08:55:18 +0900

    setlog.php modified.

--- chap13/13-4/bindValue/setlog.php
+++ chap13/13-4/bindValue/setlog.php
@@ -1,11 +1,13 @@
 <?php
 $user = "root";
-$password = "";
+$password = "root";
 $dbName = "testdb";
 $host = "localhost:3306";
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 
-
+// 1 -- general_log を有効にする
+// 0 -- general_log を無効にする
+$setLog = 0;
 try {
   $pdo = new PDO($dsn, $user, $password);
   $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -15,15 +17,15 @@ try {
   // mysql.log は自動で作成される。
   //
   // for linux and mac
-  // $pdo->exec("SET GLOBAL general_log_file = '/var/log/mysql/mysql.log'");
+  $pdo->exec("SET GLOBAL general_log_file = '/var/log/mysql/mysql.log'");
   //
   // for windows
-  $pdo->exec("SET GLOBAL general_log_file = 'c:/xampp/mysql/log/mysql.log'");
-  $pdo->exec("SET GLOBAL general_log = 1");
+  // $pdo->exec("SET GLOBAL general_log_file = 'c:/xampp/mysql/log/mysql.log'");
+  $pdo->exec("SET GLOBAL general_log = {$setLog}");
 } catch (Exception $e) {
   echo '<span class="error">エラーがありました</span><br>';
   echo $e->getMessage();
   exit();
 }
-echo 'ログを設定しました。', PHP_EOL;
+echo "ログを{$setLog}に設定しました。", PHP_EOL;
 echo 'c:\xampp\mysql\log\mysql.log';
